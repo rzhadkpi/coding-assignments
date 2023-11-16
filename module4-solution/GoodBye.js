3534fed85d11cf2927d2260bad9332f373f7e986
rzhadkpi <zhadko.ruslan@lll.kpi.ua>
2023-11-16 12:27:22 +0200

    add module4

---
+++ module4-solution/GoodBye.js
@@ -0,0 +1,9 @@
+(function(window) {
+	var speakWord = "GoodBye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
